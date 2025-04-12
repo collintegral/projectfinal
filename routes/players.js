c1908e684080339fa156ad0fec6f59dbfbb6a39d
collintegral <collintegral@gmail.com>
2025-04-12 09:20:52 -0400

    Add validation to player

--- routes/players.js
+++ routes/players.js
@@ -8,9 +8,9 @@ const validator = require('../middleware/validate.js');
 router.get('/', playerController.readAll);
 router.get('/:id', playerController.readOne);
 
-router.post('/', isAuthenticated, playerController.createOne);
+router.post('/', isAuthenticated, validator.savePlayer, playerController.createOne);
 
-router.put('/:id', isAuthenticated, playerController.updateOne);
+router.put('/:id', isAuthenticated, validator.savePlayer, playerController.updateOne);
 
 router.delete(':id', isAuthenticated, playerController.deleteOne);
 
